4fe8f274f4c0d4d4c95df818f0cbab545abce916
Stephen <hubbard_stephen@yaho.com>
2019-12-12 12:05:41 -0700

    still need some db stuff

--- src/CssStuff.js
+++ src/CssStuff.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './CssStuff.css';
+import axios from 'axios';
 
 export default class CssStuff extends Component {
         state = {
@@ -20,17 +21,28 @@ export default class CssStuff extends Component {
 
     componentDidMount() {
         setInterval(() => this.tick(), 1000);
+        this.queryFn() 
+    }
+
+    queryFn() {
+
     }
 
     componentDidUpdate() {
         console.log("component updated")
-
     }
 
     render() {
         return(
             <div>
                 <h3>{this.state.time}</h3>
+                <div className="this-is-a-class">
+                    <div id="this-is-a-id">
+                        <h5>Random H5 Tag</h5>
+                        <h6>Random H6 Tag #1</h6>
+                        <h6 className="className-1 className-2">Random H6 Tag #2</h6>
+                    </div>
+                </div>
             </div>
         )
     }
